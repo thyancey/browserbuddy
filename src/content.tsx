1bd0341c3e1bb5a6c56e29daa281cd185029f89c
Tom Yancey <thyancey84@gmail.com>
2023-09-12 17:31:56 -0400

    vite/crxjs setup

---
+++ src/content.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+const root = document.createElement('div');
+root.id = 'crx-root';
+document.body.appendChild(root);
+
+console.log('hi')
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
