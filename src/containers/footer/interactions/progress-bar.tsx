ca59470babb71ddc992de0ef25b6a60f218b11e9
Tom Yancey <thyancey84@gmail.com>
2023-09-15 03:24:43 -0400

    fix progressBar style issues

--- src/containers/footer/interactions/progress-bar.tsx
+++ src/containers/footer/interactions/progress-bar.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-type ScProgressBarProps = {
-  $startWidth: string
-}
-
 const ScWrapper = styled.div`
   position:absolute;
   left:0;
@@ -22,12 +18,11 @@ const ScProgressBg = styled.div`
   bottom:0;
 `;
 
-const ScProgressBar = styled.div<ScProgressBarProps>`
+const ScProgressBar = styled.div`
   background-color:var(--color-red);
   position:absolute;
   height:100%;
   left:0;
-  width: ${p => p.$startWidth};
 
   &.full{
     width:100% !important;
@@ -52,9 +47,8 @@ export const ProgressBar = ({startProgress, duration}: ProgressBarProps) => {
   return (
     <ScWrapper>
       <ScProgressBar 
-        $startWidth={`${startProgress * 100}%`} 
         className={loaded ? 'full' : ''} 
-        style={{ transition: `all ${duration}s linear` }} />
+        style={{ transition: `all ${duration}s linear`, width: `${startProgress * 100}%` }} />
       <ScProgressBg />
     </ScWrapper>
   )
